dba46b04510f9ecbd4e5387823104c5b3d5ff35a
muhammadatifarif <71638341+muhammadatifarif@users.noreply.github.com>
2022-06-18 17:08:36 +0500

    menu item added

--- app/src/main/java/com/inc/sada_pay_test/ui/activity/MainActivity.kt
+++ app/src/main/java/com/inc/sada_pay_test/ui/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package com.inc.sada_pay_test.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
 import com.inc.sada_pay_test.R
 import com.inc.sada_pay_test.databinding.ActivityMainBinding
@@ -30,9 +32,37 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun init() {
-
+    private fun init() {
         toolbarViewModel.title.postValue(getString(R.string.trending))
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+
+            R.id.refresh_btn -> {
+                return true
+            }
+
+            R.id.dark_theme_btn -> {
+                item.isChecked = !item.isChecked
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+
+        }
+
     }
 
 
